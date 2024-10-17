a83684e63f89deb96ae743bd7fb5319046d50677
cnhhoang <cnhhoang.main@gmail.com>
2024-10-17 19:16:49 +1100

    Code refactor

--- api/postIPAddress.js
+++ api/postIPAddress.js
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
 // ====================================================================================================
-// Function to connect to MongoDB
 async function connectToDatabase(uri) {
     const client = await MongoClient.connect(uri, {
         useNewUrlParser: true,
@@ -14,31 +13,34 @@ async function connectToDatabase(uri) {
 //****************************************************************************************************
 // API handler function
 export default async function handler(req, res) {
-  // Handle CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    // Handle CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle preflight request
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
-  if (req.method === "POST") {
-    const visitorData = req.body;
-    console.log("Received visitor data:", visitorData); // Log incoming data
-
-    try {
-        const db = await connectToDatabase(process.env.MONGODB_URI);
-        const collection = db.collection('ip');
-        const result = await collection.insertOne(visitorData);
-        res.status(200).json({ success: true, message: "Data added to MongoDB", result });
-    } catch (error) {
-      console.error("Error pushing data to MongoDB:", error); // Log the error
-      res.status(500).json({ success: false, message: "Failed to push data to MongoDB" + error});
+    // Handle preflight request
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
     }
-  } else {
-    res.status(405).json({ success: false, message: "Method not allowed" });
-  }
-}
+
+    // Handle POST request to MongoDB
+    if (req.method === "POST") {
+        const visitorData = req.body;
+        console.log("Received visitor data:", visitorData);
+
+        try {   // Try connecting to MongoDB
+            const db = await connectToDatabase(process.env.MONGODB_URI);
+            const collection = db.collection('ip');
+            const result = await collection.insertOne(visitorData);
+            res.status(200).json({ success: true, message: "Data added to MongoDB", result });
+        } 
+        catch (error) {
+            console.error("Error pushing data to MongoDB:", error); 
+            res.status(500).json({ success: false, message: "Failed to push data to MongoDB" + error});
+        }
+    } 
+    else {
+        res.status(405).json({ success: false, message: "Method not allowed" });
+    }
+};
