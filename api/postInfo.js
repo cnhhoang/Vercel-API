a08af809b055b6292d5ebf388d5cb05b9f0d5624
cnhhoang <cnhhoang.main@gmail.com>
2024-10-18 23:43:47 +1100

    Add postInfo API

---
+++ api/postInfo.js
@@ -0,0 +1,46 @@
+import { MongoClient } from "mongodb";
+
+// ====================================================================================================
+async function connectToDatabase(uri) {
+    const client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db('webpage');
+    return db;
+}
+
+//****************************************************************************************************
+// API handler function
+export default async function handler(req, res) {
+    // Handle CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Handle preflight request
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+
+    // Handle POST request to MongoDB
+    if (req.method === "POST") {
+        const visitorData = req.body;
+        console.log("Received visitor data:", visitorData);
+
+        try {   // Try connecting to MongoDB
+            const db = await connectToDatabase(process.env.MONGODB_URI);
+            const collection = db.collection('info');
+            const result = await collection.insertOne(visitorData);
+            res.status(200).json({ success: true, message: "Data added to MongoDB", result });
+        } 
+        catch (error) {
+            console.error("Error pushing data to MongoDB:", error); 
+            res.status(500).json({ success: false, message: "Failed to push data to MongoDB" + error});
+        }
+    } 
+    else {
+        res.status(405).json({ success: false, message: "Method not allowed" });
+    }
+};
